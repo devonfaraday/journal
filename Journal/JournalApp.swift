d1896c40cb6c361500c2b38328232b681855a855
Christian McMullin <christianmcmullin@gmail.com>
2021-10-04 10:39:10 -0600

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Christian McMullin on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
